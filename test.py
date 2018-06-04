99e14227e07a8243ef79f11af6f4eb7d16f88608
bandy004 <DBE2@dsone.3ds.com>
2018-06-05 08:15:54 +1000

    Practice documents

---
+++ test.py
@@ -0,0 +1,15 @@
+def test1():
+	return print("Test1");
+
+
+def test2():
+	return print("Test2");
+	
+def test3():
+	return print("Test3");
+	
+class Test:
+	def __init__(self, name):
+		self.name = name;
+		print("test created", self.name)
+
---
+++ test_test.py
@@ -0,0 +1,11 @@
+import test
+
+test.test1()
+test.test2()
+test.test3()
+
+a = test;
+a.test2()
+
+b = test.Test("Debdeep")
+print(b.name)
